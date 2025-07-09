d2083af20ba4ee96c71f5ffc154eb0e572f15ee0
Jitender Kumar <thejitenderk@gmail.com>
2025-07-09 14:03:33 -0400

    bug fix in sql databast

--- module/sql-database/main.tf
+++ module/sql-database/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mssql_database" "sql_database" {
   for_each = var.sql_database
   name         = each.value.name
-  server_id    = data.azurerm_mssql_server.datasqlserver.id
+  server_id    = data.azurerm_mssql_server.datasqlserver[each.key].id
   collation    = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
   max_size_gb  = 2

0971b0fbfdc59dc0ab1eb4b1a3c7d88a6a5522fe
Jitender Kumar <thejitenderk@gmail.com>
2026-01-19 10:17:26 +0000

    module path corected

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,43 +1,43 @@
 module "rgs" {
-  source = "../module/resource-group"
+  source = "../../module/resource-group"
   rgname = var.rg_name
 }
 
 module "vnets" {
   depends_on = [module.rgs]
-  source     = "../module/virtual-network"
+  source     = "../../module/virtual-network"
   vnet       = var.vnet_name
 
 }
 
 module "subnets" {
   depends_on = [module.vnets]
-  source     = "../module/subnet"
+  source     = "../../module/subnet"
   subnetname = var.subnet_name
 }
 
 
 module "pips" {
   depends_on = [module.rgs]
-  source     = "../module/public-ip"
+  source     = "../../module/public-ip"
   publicip   = var.pip
 }
 
 module "vms" {
   depends_on = [module.pips, module.subnets, module.rgs]
-  source     = "../module/virtual-machine"
+  source     = "../../module/virtual-machine"
   lvmm       = var.lvmms
 }
 
 
 module "nsg" {
   depends_on = [module.rgs]
-  source     = "../module/nsg"
+  source     = "../../module/nsg"
   nsg        = var.nsg
 }
 
 module "nicnsgass" {
   depends_on = [module.nsg, module.rgs, module.vms]
-  source     = "../module/subnet-network-association"
+  source     = "../../module/subnet-network-association"
   nicnsgass  = var.nicnsgass
 }
